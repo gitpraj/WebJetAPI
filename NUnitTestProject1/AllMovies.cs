e808f04e00d7fd046300378cb960525d5dff20f4
Prajith Maniyan <prajith.manian@gmail.com>
2019-06-17 02:20:55 +0000

    More unit tests

--- NUnitTestProject1/AllMovies.cs
+++ NUnitTestProject1/AllMovies.cs
@@ -67,5 +67,91 @@ namespace Tests
             int count = movieList.Count();
             Assert.AreEqual(2, count);
         }
+
+        /* movies that have "Star Wars". This test checks the number of movies that are returned  */
+        [Test]
+        public void Test4()
+        {
+            var config = InitConfiguration();
+            IEnumerable<MovieSummary> movies;
+            CinemaWorld cinemaWorld = new CinemaWorld(config.GetSection("CinemaWorldApi").Value, config.GetSection("CinemaWorldAccessToken").Value);
+            FilmWorld filmWorld = new FilmWorld(config.GetSection("FilmWorldApi").Value, config.GetSection("FilmWorldAccessToken").Value);
+            Intermediary im = new Intermediary(cinemaWorld, filmWorld);
+            movies = im.FindMovies("Star Wars").Result;
+
+            List<MovieSummary> movieList = movies.ToList();
+            var ret = movieList.Where(m => m.Title.ToLower().Contains("Star Wars".ToLower(), StringComparison.OrdinalIgnoreCase)).ToList();
+            int count = ret.Count();
+            Assert.AreEqual(13, count);
+        }
+
+        /* movies that have "John wick". This test checks the number of movies that are returned  */
+        [Test]
+        public void Test5()
+        {
+            var config = InitConfiguration();
+            IEnumerable<MovieSummary> movies;
+            CinemaWorld cinemaWorld = new CinemaWorld(config.GetSection("CinemaWorldApi").Value, config.GetSection("CinemaWorldAccessToken").Value);
+            FilmWorld filmWorld = new FilmWorld(config.GetSection("FilmWorldApi").Value, config.GetSection("FilmWorldAccessToken").Value);
+            Intermediary im = new Intermediary(cinemaWorld, filmWorld);
+            movies = im.FindMovies("John wick").Result;
+
+            List<MovieSummary> movieList = movies.ToList();
+            var ret = movieList.Where(m => m.Title.ToLower().Contains("John Wick".ToLower(), StringComparison.OrdinalIgnoreCase)).ToList();
+            int count = ret.Count();
+            Assert.AreEqual(0, count);
+        }
+
+        /* movies that have "new hope". This test checks the number of movies that are returned  */
+        [Test]
+        public void Test6()
+        {
+            var config = InitConfiguration();
+            IEnumerable<MovieSummary> movies;
+            CinemaWorld cinemaWorld = new CinemaWorld(config.GetSection("CinemaWorldApi").Value, config.GetSection("CinemaWorldAccessToken").Value);
+            FilmWorld filmWorld = new FilmWorld(config.GetSection("FilmWorldApi").Value, config.GetSection("FilmWorldAccessToken").Value);
+            Intermediary im = new Intermediary(cinemaWorld, filmWorld);
+            movies = im.FindMovies("new hope").Result;
+
+            List<MovieSummary> movieList = movies.ToList();
+            var ret = movieList.Where(m => m.Title.ToLower().Contains("new hope".ToLower(), StringComparison.OrdinalIgnoreCase)).ToList();
+            int count = ret.Count();
+            Assert.AreEqual(2, count);
+        }
+
+        /* movies that have "" empty string. This test checks the number of movies that are returned. This scenario would not
+         * occur if using the UI. */
+        [Test]
+        public void Test7()
+        {
+            var config = InitConfiguration();
+            IEnumerable<MovieSummary> movies;
+            CinemaWorld cinemaWorld = new CinemaWorld(config.GetSection("CinemaWorldApi").Value, config.GetSection("CinemaWorldAccessToken").Value);
+            FilmWorld filmWorld = new FilmWorld(config.GetSection("FilmWorldApi").Value, config.GetSection("FilmWorldAccessToken").Value);
+            Intermediary im = new Intermediary(cinemaWorld, filmWorld);
+            movies = im.FindMovies("").Result;
+
+            List<MovieSummary> movieList = movies.ToList();
+            var ret = movieList.Where(m => m.Title.ToLower().Contains("".ToLower(), StringComparison.OrdinalIgnoreCase)).ToList();
+            int count = ret.Count();
+            Assert.AreEqual(13, count);
+        }
+
+        /* movies that have "" empty string. This test checks the number of movies that are returned. This scenario would
+         * occur if enter is pressed without typing anything on the search bar. */
+        [Test]
+        public void Test8()
+        {
+            var config = InitConfiguration();
+            IEnumerable<MovieSummary> movies;
+            CinemaWorld cinemaWorld = new CinemaWorld(config.GetSection("CinemaWorldApi").Value, config.GetSection("CinemaWorldAccessToken").Value);
+            FilmWorld filmWorld = new FilmWorld(config.GetSection("FilmWorldApi").Value, config.GetSection("FilmWorldAccessToken").Value);
+            Intermediary im = new Intermediary(cinemaWorld, filmWorld);
+            movies = im.FindMovies(null).Result;
+
+            List<MovieSummary> movieList = movies.ToList();
+            int count = movieList.Count();
+            Assert.AreEqual(0, count);
+        }
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ dotnet run
 Run the specified project:
 
 dotnet run --project ./WebApplication1/WebApplication1.csproj
+
+Unit Testing:
+
+Used visual studio to run the tests.
